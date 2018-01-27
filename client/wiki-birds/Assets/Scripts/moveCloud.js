de3d59561d7cb78b1fb730715fda1ef746371bec
Harri Dammert <harry.damm@gmail.com>
2018-01-27 21:12:14 +0200

    Harry's amazing moving clouds in their own scene

---
+++ client/wiki-birds/Assets/Scripts/moveCloud.js
@@ -0,0 +1,20 @@
+    var speed: float;
+    var offscreen: float;
+    var spawn: float;
+    var highest: float;
+    var lowest: float;
+     
+     
+    function Update () {
+    //amount to move cloud
+    amtToMove = Time.deltaTime *speed;
+    //move enemy
+    transform.Translate(Vector3.right * amtToMove);
+    //respawn with random Y
+    if (transform.position.x > offscreen) {
+       
+        transform.position.x = spawn;
+        transform.position.y = Random.Range (highest , lowest);
+     
+        }
+    }
