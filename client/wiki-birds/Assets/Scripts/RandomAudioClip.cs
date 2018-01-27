e1ed9d8dc8de32ab6e7c0a864d0afa8cdb4fd688
Henri Niva <hegemege@gmail.com>
2018-01-27 12:24:26 +0200

    Random audio clip script

---
+++ client/wiki-birds/Assets/Scripts/RandomAudioClip.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAudioClip : MonoBehaviour
+{
+    private AudioSource source;
+
+    public List<AudioClip> Clips;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PlayRandom()
+    {
+        if (source.isPlaying)
+        {
+            source.Stop();
+        }
+
+        var index = Random.Range(0, Clips.Count);
+        var clip = Clips[index];
+
+        source.clip = clip;
+        source.Play();
+    }
+}
