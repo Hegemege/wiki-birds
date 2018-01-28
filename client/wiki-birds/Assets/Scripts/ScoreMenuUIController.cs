02b6804ba4a5cc5503b7b93744239fc333058992
Henri Niva <hegemege@gmail.com>
2018-01-28 13:14:41 +0200

    Score texts

--- client/wiki-birds/Assets/Scripts/ScoreMenuUIController.cs
+++ client/wiki-birds/Assets/Scripts/ScoreMenuUIController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -16,8 +18,13 @@ public class ScoreMenuUIController : MonoBehaviour
     private Coroutine outer;
     private Coroutine inner;
 
+    private bool _gotData;
+
     void Awake()
     {
+        PlayerText.text = "";
+        ScoreText.text = "";
+
         if (GameManager.Instance.RoundNumber < 3)
         {
             NextButton.SetActive(GameManager.Instance.IsHost);
@@ -42,7 +49,45 @@ public class ScoreMenuUIController : MonoBehaviour
 
     public void HandleScoreInfo(JObject data)
     {
+        try
+        {
+            if (!_gotData)
+            {
+                _gotData = true;
 
+                var players = data["players"].ToObject<List<JObject>>();
+
+                var colorNames = players.Select(player => new
+                {
+                    Color = player["color"].ToObject<string>(),
+                    Name = player["name"].ToObject<string>()
+                }).ToList();
+
+                var colorScores = players.Select(player => new
+                {
+                    Color = player["color"].ToObject<string>(),
+                    Score = player["points"].ToObject<string>()
+                }).ToList();
+
+                foreach (var name in colorNames)
+                {
+                    if (name.Color == GameManager.Instance.PlayerColor)
+                    {
+                        PlayerText.text += "(YOU) ";
+                    }
+
+                    PlayerText.text += name.Name + "\n";
+
+                    // Score text
+                    var rowScore = colorScores.Single(score => score.Color == name.Color);
+                    ScoreText.text += rowScore.Score + "\n";
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+
+        }
     }
 
     public void NextPress()
